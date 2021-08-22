raspiPardus/image-specs

commit feb61f3361ea2db92b5f12a68f1365cbee2799b4

Fix rpi-generate-ssh-host-keys.service to use the correct package name in buster

Makefile
@@ -29,6 +29,7 @@ raspi_base_buster.yaml: raspi_master.yaml
 	sed "s/__FIRMWARE_PKG__/raspi3-firmware/" | \
 	sed "s/__RELEASE__/buster/" |\
 	sed "s/__SECURITY_SUITE__/buster\/updates/" |\
+	sed "s/__FIX_FIRMWARE_PKG_NAME__/sed -i s\/raspi-firmware\/raspi3-firmware\/ \/etc\/systemd\/system\/rpi-reconfigure-raspi-firmware.service/" |\
 	grep -v '__EXTRA_SHELL_CMDS__' > $@
 
 raspi_1_buster.yaml: raspi_base_buster.yaml
@@ -79,6 +80,7 @@ raspi_base_bullseye.yaml: raspi_master.yaml
 	sed "s/__RELEASE__/bullseye/" |\
 	sed "s/__FIRMWARE_PKG__/raspi-firmware/" | \
 	grep -v "__OTHER_APT_ENABLE__" |\
+	grep -v "__FIX_FIRMWARE_PKG_NAME__" |\
 	sed "s/__SECURITY_SUITE__/bullseye-security/" > $@
 
 raspi_1_bullseye.yaml: raspi_base_bullseye.yaml

raspi_master.yaml
@@ -115,6 +115,7 @@ steps:
       install -m 644 -o root -g root rootfs/etc/systemd/system/rpi-reconfigure-raspi-firmware.service "${ROOT?}/etc/systemd/system/"
       mkdir -p "${ROOT?}/etc/systemd/system/multi-user.target.requires/"
       ln -s /etc/systemd/system/rpi-reconfigure-raspi-firmware.service "${ROOT?}/etc/systemd/system/multi-user.target.requires/rpi-reconfigure-raspi-firmware.service"
+      __FIX_FIRMWARE_PKG_NAME__
 
       install -m 644 -o root -g root rootfs/etc/systemd/system/rpi-generate-ssh-host-keys.service "${ROOT?}/etc/systemd/system/"
       ln -s /etc/systemd/system/rpi-generate-ssh-host-keys.service "${ROOT?}/etc/systemd/system/multi-user.target.requires/rpi-generate-ssh-host-keys.service"

commit 3058cf8e8289c9dba69f635384375d50972382db

Run sed on rpi-reconfigure-raspi-firmware.service within $ROOT.
This fixes the build of all buster-based images (regression in
feb61f3361ea2db92b5f12a68f1365cbee2799b4).

Makefile
@@ -29,7 +29,7 @@ raspi_base_buster.yaml: raspi_master.yaml
 	sed "s/__FIRMWARE_PKG__/raspi3-firmware/" | \
 	sed "s/__RELEASE__/buster/" |\
 	sed "s/__SECURITY_SUITE__/buster\/updates/" |\
-	sed "s/__FIX_FIRMWARE_PKG_NAME__/sed -i s\/raspi-firmware\/raspi3-firmware\/ \/etc\/systemd\/system\/rpi-reconfigure-raspi-firmware.service/" |\
+	sed "s/__FIX_FIRMWARE_PKG_NAME__/sed -i s\/raspi-firmware\/raspi3-firmware\/ \$${ROOT?}\/etc\/systemd\/system\/rpi-reconfigure-raspi-firmware.service/" |\
 	grep -v '__EXTRA_SHELL_CMDS__' > $@
 
 raspi_1_buster.yaml: raspi_base_buster.yaml