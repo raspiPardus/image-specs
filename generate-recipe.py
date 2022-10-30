raspiPardus/image-specs

commit 1ffce8e6bb75dd8f2fff33d5e760184956502d10

For Bookworm, use the new non-free-firmware section instead of non-free

generate-recipe.py
@@ -50,6 +50,19 @@ else:
     raspi_firmware = 'raspi-firmware'
     fix_firmware = False
 
+# Bookworm introduced the 'non-free-firmware' component¹; before that,
+# raspi-firmware was in 'non-free'
+#
+# ¹ https://www.debian.org/vote/2022/vote_003
+if suite == 'bookworm':
+    firmware_component = 'non-free-firmware'
+else:
+    # We only need "non-free". "Contrib" was before included as well,
+    # as they are usually enabled together, but adding a second YAML
+    # element... makes the resulting code ugly and brittle. Besides,
+    # we don't use anything from contrib!
+    firmware_component = 'non-free'
+
 # Extra wireless firmware:
 if version != '2':
     wireless_firmware = 'firmware-brcm80211'
@@ -108,14 +121,15 @@ else:
 if backports_enable:
     backports_stanza = """
 %s
-deb http://deb.debian.org/debian/ %s main contrib non-free
-""" % (backports_enable, backports_suite)
+deb http://deb.debian.org/debian/ %s main %s
+""" % (backports_enable, backports_suite, firmware_component)
 else:
+    # ugh
     backports_stanza = """
 # Backports are _not_ enabled by default.
 # Enable them by uncommenting the following line:
-# deb http://deb.debian.org/debian %s main contrib non-free
-""" % backports_suite
+# deb http://deb.debian.org/debian %s main %s
+""" % (backports_suite, firmware_component)
 
 # Buster requires an existing, empty /etc/machine-id file:
 if suite == 'buster':
@@ -159,6 +173,7 @@ with open('raspi_master.yaml', 'r') as in_file:
         out_text = in_text \
             .replace('__RELEASE__', suite) \
             .replace('__ARCH__', arch) \
+            .replace('__FIRMWARE_COMPONENT__', firmware_component) \
             .replace('__LINUX_IMAGE__', linux) \
             .replace('__DTB__', dtb) \
             .replace('__SECURITY_SUITE__', security_suite) \

raspi_master.yaml
@@ -45,14 +45,13 @@ steps:
     arch: __ARCH__
     components:
     - main
-    - contrib
-    - non-free
+    - __FIRMWARE_COMPONENT__
     unless: rootfs_unpacked
 
   - create-file: /etc/apt/sources.list
     contents: |+
-      deb http://deb.debian.org/debian __RELEASE__ main contrib non-free
-      deb http://security.debian.org/debian-security __SECURITY_SUITE__ main contrib non-free
+      deb http://deb.debian.org/debian __RELEASE__ main __FIRMWARE_COMPONENT__
+      deb http://security.debian.org/debian-security __SECURITY_SUITE__ main __FIRMWARE_COMPONENT__
       __BACKPORTS__
 
     unless: rootfs_unpacked

commit d71140b08a4d16d7df3fb44026df060d37909cfe

Move 'contrib' removal comment to git commit msg
Previous commit not only replaced 'non-free' with 'non-free-firmware',
it also removed the 'contrib' archive area from the sources.list.
It added a note about it in the code comments, which I think is the
wrong place. The 'why' of a change belongs in a git commit message,
where one can be as verbose as needed.
Code comments should be used to clarify the 'what' (it does) in case it
would not be immediately obvious.

The removal of 'contrib' totally makes sense though.
We did not use it and 'contrib' and 'non-free' are not part of
(official) Debian, whereas 'non-free-firmware' is now part of Debian
(official media) as a consequence of the change to the Debian Social
Contract following the GR vote.
With this change, we only use what Debian itself would only use.

Fixes: 1ffce8e6bb75dd8f2fff33d5e760184956502d10

generate-recipe.py
@@ -57,10 +57,6 @@ else:
 if suite == 'bookworm':
     firmware_component = 'non-free-firmware'
 else:
-    # We only need "non-free". "Contrib" was before included as well,
-    # as they are usually enabled together, but adding a second YAML
-    # element... makes the resulting code ugly and brittle. Besides,
-    # we don't use anything from contrib!
     firmware_component = 'non-free'
 
 # Extra wireless firmware: